7ed126e0dda165887c9ddeb742e9c19588dc0d68
Molly Kent <kent.molly@gmail.com>
2015-10-16 08:42:13 -0700

    Fix sass gulp task

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,21 +1,16 @@
 var gulp = require('gulp');
-var config = require('../config').sass;
-
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var handleErrors = require('../util/handleErrors');
 var autoprefixer = require('gulp-autoprefixer');
-
-
-var dest = './public';
-var src = './src';
+var config = require('../config').sass;
 
 gulp.task('sass', function() {
-  return gulp.src('./app/style/**/*.{sass,scss}')
+  return gulp.src(config.src)
     .pipe(sourcemaps.init())
     .pipe(sass(config.settings))
     .on('error', handleErrors)
     .pipe(sourcemaps.write())
     .pipe(autoprefixer({ browsers: ['last 2 version'] }))
-    .pipe(gulp.dest('build/style/'));
+    .pipe(gulp.dest(config.dest));
 });
